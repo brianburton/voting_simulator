c91eefdc634e91124e25b5fee36a6d4e5997cf90
Brian Burton <brian@burton-computer.com>
2022-03-28 15:48:58 -0500

    Increases random bias when computing voter center.

--- src/main/java/com/burtonzone/election/IssueSpace.java
+++ src/main/java/com/burtonzone/election/IssueSpace.java
@@ -12,6 +12,7 @@ public abstract class IssueSpace
     protected static final int MaxVoterDistance = 45;
     protected static final int PartyPositionBias = 1;
     protected static final int VoterPositionBias = 2;
+    protected static final int VoterCenterBias = 4;
     protected static final int CandidatePositionBias = 3;
     protected static final JImmutableList<Integer> PartyPoints = list(10, 15, 20, 25, 30, 35, 40, 45, 55, 60, 65, 70, 75, 80, 85, 90);
     protected static final JImmutableList<Integer> CenterPartyPoints = list(35, 40, 45, 55, 60, 65);
@@ -36,7 +37,7 @@ public abstract class IssueSpace
     public Position voterCenterPosition(JImmutableList<Party> parties)
     {
         final var positions = parties.transform(Party::getPosition);
-        return positions.get(0).somewhereIn(rand, 2, positions);
+        return positions.get(0).somewhereIn(rand, VoterCenterBias, positions);
     }
 
     public Position voterPosition(Position voterCenterPosition)
