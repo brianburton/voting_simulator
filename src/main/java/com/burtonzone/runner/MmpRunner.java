a24c5d891a5d7c10ee58479719031a74746677a5
Brian Burton <brian@burton-computer.com>
2022-04-23 14:16:20 -0400

    Refactoring.

--- src/main/java/com/burtonzone/runner/MmpRunner.java
+++ src/main/java/com/burtonzone/runner/MmpRunner.java
@@ -15,8 +15,9 @@ import com.burtonzone.election.ElectionRunner;
 import com.burtonzone.election.Party;
 import java.math.BigDecimal;
 import java.util.function.IntUnaryOperator;
-import lombok.Value;
+import lombok.AllArgsConstructor;
 import org.javimmutable.collections.JImmutableList;
+import org.javimmutable.collections.JImmutableListMap;
 import org.javimmutable.collections.JImmutableSet;
 
 public class MmpRunner
@@ -49,7 +50,7 @@ public class MmpRunner
         final var regionalElections = elections.splitRegions();
         RegionalResults combinedResults = null;
         for (Elections regionalElection : regionalElections) {
-            final var regionalResults = runRegionalElections(regionalElection);
+            final var regionalResults = runDistrictElections(regionalElection);
             if (combinedResults == null) {
                 combinedResults = regionalResults;
             } else {
@@ -60,17 +61,37 @@ public class MmpRunner
         return combinedResults.toElectionResults(elections.isParallel());
     }
 
-    private RegionalResults runRegionalElections(Elections elections)
+    private RegionalResults runDistrictElections(Elections elections)
+    {
+        ensureAllElectionsAreSingleSeat(elections);
+        final var details = gatherElectionDetails(elections);
+        final var pluralityResults = districtRunner.runElections(elections);
+
+        final var districtWinners = pluralityResults.getResults().stream()
+            .flatMap(er -> er.getElectedVotes().stream())
+            .map(CandidateVotes::getCandidate)
+            .collect(listCollector());
+
+        final var partyResult = runPartyListElection(details, districtWinners);
+        final var effectiveResults = combineResults(details, districtWinners, partyResult);
+        return new RegionalResults(elections.getElections(), pluralityResults.getResults(), list(effectiveResults));
+    }
+
+    private void ensureAllElectionsAreSingleSeat(Elections elections)
     {
         for (Election election : elections.getElections()) {
             if (election.getSeats() != 1) {
                 throw new IllegalArgumentException("MMP requires single member districts");
             }
         }
+    }
 
-        final var pluralityResults = districtRunner.runElections(elections);
+    private MmpElectionDetails gatherElectionDetails(Elections elections)
+    {
         final var districts = elections.getElections();
-        final var parties = districts.get(0).getParties();
+        final var parties = districts.stream()
+            .flatMap(d -> d.getParties().stream())
+            .collect(listCollector());
         final var candidates = districts.stream()
             .flatMap(e -> e.getExpandedCandidateList().stream())
             .collect(listCollector());
@@ -81,51 +102,49 @@ public class MmpRunner
             .mapToInt(Election::getSeats)
             .sum();
         final var seats = seatsCalculator.applyAsInt(districtSeats);
-
-        final var districtWinners = pluralityResults.getResults().stream()
-            .flatMap(er -> er.getElectedVotes().stream())
-            .map(CandidateVotes::getCandidate)
-            .collect(listCollector());
-
-        final var filteredParties = computePartiesToFilter(partyVotes, districtWinners);
+        final int listSeats = seats - districtSeats;
         final var ballots = districts
             .reduce(BallotBox.Empty, (sum, e) -> sum.add(e.getBallots()))
             .toSingleChoiceBallots();
-        final var partyBallots = ballots
-            .withoutBallotsMatching(b -> b.partyIn(filteredParties));
-        final var partyElection = new Election("", parties, candidates, list(), partyLists, partyBallots, seats);
-        final var partyResult = partyRunner.runMppPartyElection(partyElection, districtWinners);
+        return new MmpElectionDetails(parties, candidates, partyLists, partyVotes, districtSeats, listSeats, seats, ballots);
+    }
 
+    private ElectionResult runPartyListElection(MmpElectionDetails details,
+                                                JImmutableList<Candidate> districtWinners)
+    {
+        final var filteredParties = computePartiesToFilter(details.partyVotes, districtWinners);
+        final var partyBallots = details.ballots.withoutBallotsMatching(b -> b.partyIn(filteredParties));
+        final var partyElection = details.toElection(partyBallots);
+        return partyRunner.runMppPartyElection(partyElection, districtWinners);
+    }
+
+    private ElectionResult combineResults(MmpElectionDetails details,
+                                          JImmutableList<Candidate> districtWinners,
+                                          ElectionResult partyResult)
+    {
         int votedCount = CandidateVotes.countType(partyResult.getElectedVotes(), CandidateVotes.SelectionType.Vote);
         int listCount = CandidateVotes.countType(partyResult.getElectedVotes(), CandidateVotes.SelectionType.List);
         if (votedCount != districtWinners.size()) {
             throw new IllegalArgumentException("voted count mismatch");
         }
-        if (seats != votedCount + listCount) {
-            throw new IllegalArgumentException("count mismatch");
+        if (votedCount != details.districtSeats) {
+            throw new IllegalArgumentException("district count mismatch");
         }
-        if (listCount != seats - districtSeats) {
+        if (listCount != details.listSeats) {
             throw new IllegalArgumentException("list count mismatch");
         }
         final var winningCandidates = set(districtWinners);
         final var winningParties = partyResult.getElectedVotes()
             .transform(set(), cv -> cv.getCandidate().getParty());
-        final var effectiveElection = new Election("", parties, candidates, list(), partyLists, ballots, seats);
-        final var wasted = ballots.countWastedUsingCandidateOrParty(partyResult.getElectedVotes());
-        final var effectiveVoteScore = OpenListRunner.computeEffectiveVoteScore(ballots, winningCandidates, winningParties);
-        final var effectiveResults = new ElectionResult(effectiveElection,
-                                                        ballots,
-                                                        partyVotes,
-                                                        wasted,
-                                                        effectiveVoteScore,
-                                                        partyResult.getElectedVotes());
-        if (CandidateVotes.countType(effectiveResults.getElectedVotes(), CandidateVotes.SelectionType.Vote) != votedCount) {
-            throw new IllegalArgumentException("voted count mismatch");
-        }
-        if (CandidateVotes.countType(effectiveResults.getElectedVotes(), CandidateVotes.SelectionType.List) != listCount) {
-            throw new IllegalArgumentException("count mismatch");
-        }
-        return new RegionalResults(elections.getElections(), pluralityResults.getResults(), list(effectiveResults));
+        final var effectiveElection = details.toElection(details.ballots);
+        final var wasted = details.ballots.countWastedUsingCandidateOrParty(partyResult.getElectedVotes());
+        final var effectiveVoteScore = OpenListRunner.computeEffectiveVoteScore(details.ballots, winningCandidates, winningParties);
+        return new ElectionResult(effectiveElection,
+                                  details.ballots,
+                                  details.partyVotes,
+                                  wasted,
+                                  effectiveVoteScore,
+                                  partyResult.getElectedVotes());
     }
 
     private JImmutableSet<Party> computePartiesToFilter(Counter<Party> realVotes,
@@ -157,12 +176,30 @@ public class MmpRunner
             .sum();
     }
 
-    @Value
+    @AllArgsConstructor
+    private static class MmpElectionDetails
+    {
+        private final JImmutableList<Party> parties;
+        private final JImmutableList<Candidate> candidates;
+        private final JImmutableListMap<Party, Candidate> partyLists;
+        private final Counter<Party> partyVotes;
+        private final int districtSeats;
+        private final int listSeats;
+        private final int seats;
+        private final BallotBox ballots;
+
+        private Election toElection(BallotBox ballots)
+        {
+            return new Election("", parties, candidates, list(), partyLists, ballots, seats);
+        }
+    }
+
+    @AllArgsConstructor
     private static class RegionalResults
     {
-        JImmutableList<Election> elections;
-        JImmutableList<ElectionResult> pluralityResults;
-        JImmutableList<ElectionResult> partyResults;
+        private final JImmutableList<Election> elections;
+        private final JImmutableList<ElectionResult> pluralityResults;
+        private final JImmutableList<ElectionResult> partyResults;
 
         private RegionalResults add(RegionalResults other)
         {
